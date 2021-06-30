570528c85d39712197dae91f80831a35dda06135
AbhikPatra97 <abhik.patra2018@gmail.com>
2021-06-30 21:34:18 +0530

    Prime Number

---
+++ Prime Number/src/PrimeNumber/PrimeNumber.java
@@ -0,0 +1,28 @@
+package PrimeNumber;
+
+public class PrimeNumber {
+	public static boolean isprime(int n)
+	{
+		if(n<=1)
+			return false;
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			if(isprime(i))
+				System.out.println(i);
+		}
+	}
+	
+
+}
+
